54372bbbe8293f4a84cacb3cc374d0b8281c2033
ShwetaPal <shwetapal975@gmail.com>
2024-02-19 22:26:47 +0530

    insert at a pos

--- insertionInLL.js
+++ insertionInLL.js
@@ -41,11 +41,32 @@ class LL {
   size() {
     let count = 0;
     let curr = this.head;
-    while (curr) {
+    while (curr.next) {
       count++;
       curr = curr.next;
     }
     return count;
+  }
+
+  // Insert at a particular Index
+
+  addAtIndex(index, data) {
+    if (index < 0 || index > this.size()) {
+      console.log("Invalid Index");
+      return;
+    }
+    const node = new Node(data);
+    if (index === 0) {
+      node.next = this.head;
+      this.head = node;
+      return;
+    }
+    let curr = this.head;
+    for (let i = 0; i < index - 1; i++) {
+      curr = curr.next;
+    }
+    node.next = curr.next;
+    curr.next = node;
   }
 
   //Function to print he Linked List
@@ -66,5 +87,7 @@ l1.insertAtStart(9);
 l1.insertAtStart(1);
 l1.addAtEnd(2);
 l1.addAtEnd(3);
+l1.addAtIndex(5, 12);
+l1.addAtIndex(8, 50);
 l1.printLL();
 console.log(l1.size());
